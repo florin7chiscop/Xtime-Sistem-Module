6bf4cf822dc8a70cc4b5b7aeff24d2749eacd111
florin7chiscop <florin.chiscop231@gmail.com>
2025-02-17 22:06:29 +0200

    initial.bash

---
+++ initial.bash
@@ -0,0 +1,7 @@
+# Clonezi repository-ul
+git clone https://github.com/florin7chiscop/multiversx-smart-contract.git
+cd multiversx-smart-contract
+
+# Inițializezi un nou proiect MultiversX Smart Contract
+mxpy contract new my_contract
+cd my_contract
