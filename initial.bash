4e010dc17de0b40b36c0e036165bc3286b226659
florin7chiscop <florin.chiscop231@gmail.com>
2025-02-17 22:18:02 +0200

    initial.sh

--- initial.bash
+++
@@ -1,9 +0,0 @@
-# Clonezi repository-ul
-git clone https://github.com/user/multiversx-smart-contract.git
-cd multiversx-smart-contract
-
-# Inițializezi un nou proiect MultiversX Smart Contract
-mxpy contract new my_contract
-cd my_contract
-
-
---
+++ initial.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/florin7chiscop/Xtime-Sistem-Module.git
+cd Xtime-Sistem-Module
+
